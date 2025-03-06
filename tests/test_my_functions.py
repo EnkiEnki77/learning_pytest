df3cafedfb9b058b1899f73af624f2c5cc12109e
Enki Layman <layman212@gmail.com>
2025-03-06 10:58:41 -0500

    Learned about adding meta data to your tests using marking

--- tests/test_my_functions.py
+++ tests/test_my_functions.py
@@ -1,3 +1,4 @@
+import time
 import pytest
 import Source.my_functions as my_functions
 
@@ -23,4 +24,22 @@ def test_divide_by_zero():
     # To test for Exceptions use the following syntax:
     with pytest.raises(ZeroDivisionError):
         result = my_functions.divide(4, 0)
+
+# Marks the test with a slow tag, to specifically run tests with a certain mark use -m mark in the terminal
+@pytest.mark.slow
+def test_very_slow():
+    # Setups a function that stops execution for 5 seconds
+    time.sleep(5)
+    result = my_functions.add(1, 4)
+    assert result == 5
+
+@pytest.mark.skip(reason="this feature is dumb.")
+def test_dumb():
+    result = my_functions.add(1, 4)
+    assert result == 5
+
+# We know test will fail and dont want to fix it for some reason.
+@pytest.mark.xfail(reason="We know we cant divide by zero.")
+def test_divide_by_zero():
+    my_functions.divide(4, 2)
 
